ac1ef782af92302fff4f3369fa97fc30bbebf7e4
Mason Sain <sainmason012@gmail.com>
2024-04-21 03:41:37 -0400

    Added first apply page

--- index.php
+++ index.php
@@ -14,20 +14,52 @@ $f3 = Base::instance();
 
 // Define a default route
 $f3->route('GET /', function() {
-    //echo '<h1>Application Home</h1>';
 
     // Render a view page
     $view = new Template();
     echo $view->render('views/home.html');
 });
 
-// Define a default route
-$f3->route('GET|POST /apply1', function() {
+// Define a route to apply1
+$f3->route('GET|POST /apply1', function($f3) {
+    session_start();
+
+    //Check if the form has been posted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $fname = $_POST['first-name'];
+        $lname = $_POST['last-name'];
+        $email = $_POST['user-email'];
+        $state = $_POST['user-state'];
+        $phone = $_POST['phone-num'];
+        //Validate the data
+        if (empty($fname) || empty($lname) || empty($email) || empty($phone)) {
+            //Data is invalid
+            echo "Please fill out the data";
+        } else {
+            //Data is valid
+            $f3->set('SESSION.user-first-name', $fname);
+            $f3->set('SESSION.user-last-name', $lname);
+            $f3->set('SESSION.user-email', $email);
+            $f3->set('SESSION.user-state', $state);
+            $f3->set('SESSION.user-phone', $phone);
+            //Redirect to the summary route
+            $f3->reroute("apply2");
+        }
+    }
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/app-personal-info.html');
+});
+
+// Define a route to apply2
+$f3->route('GET|POST /apply2', function() {
+    session_start();
+
     echo '<h1>Application Apply!</h1>';
 
     // Render a view page
     //$view = new Template();
-    //echo $view->render('views/home.html');
+    //echo $view->render('views/app-personal-info.html');
 });
 
 // Run Fat-Free
